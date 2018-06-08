d9203a7c16180084a1353652fd751526722b5fa7
BartheqVonKoss <37189954+BartheqVonKoss@users.noreply.github.com>
2018-06-08 18:24:28 +0200

    Create main.cpp

---
+++ L3/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  List3
+//
+//  Created by Bartłomiej Kos on 14/05/2018.
+//  Copyright © 2018 Bartłomiej Kos. All rights reserved.
+//
+
+#include <iostream>
+#include "req.hpp"
+
+int main(int argc, const char * argv[])
+{
+    charac();
+    return 0;
+}
