748e4a1657934ef516c0bd7e29545becce6bdacd
BartheqVonKoss <37189954+BartheqVonKoss@users.noreply.github.com>
2018-06-08 18:25:47 +0200

    Create req.hpp

---
+++ L3/req.hpp
@@ -0,0 +1,17 @@
+//
+//  req.hpp
+//  List3
+//
+//  Created by Bartłomiej Kos on 14/05/2018.
+//  Copyright © 2018 Bartłomiej Kos. All rights reserved.
+//
+
+#ifndef req_hpp
+#define req_hpp
+
+#include <stdio.h>
+
+double f(double x);
+void charac();
+
+#endif /* req_hpp */
